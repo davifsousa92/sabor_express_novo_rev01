2ecf7fb043ed540c812ccd313d62133a6fef51fe
Davi Ferreira <davi.fsousa92@hotmail.com>
2024-07-01 16:56:10 -0300

    aula04-listando avaliacoes

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from modelos.restaurante import Restaurante
 restaurante_praca = Restaurante('praça','Gourmet')
 restaurante_praca.receber_avaliacao('Gui', 10)
 restaurante_praca.receber_avaliacao('Lais', 8)
-restaurante_praca.receber_avaliacao('Amy', 5)
+restaurante_praca.receber_avaliacao('Amy', 7)
 
 
 def main():
